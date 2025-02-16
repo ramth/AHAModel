0c8ca066d3696ff02d22a0465b3a5c6fe08dd7c8
ramth <ram.rajagopal93@gmail.com>
2025-02-16 00:06:49 -0500

    Update ic_gen.py comments

--- ahamodel/ic_gen.py
+++ ahamodel/ic_gen.py
@@ -1,7 +1,17 @@
+## ---------------------------
+## Lattice initial condition functions
+##
+## Authors: Ramprasath Rajagopal
+##
+## Email: rrajagop@bu.edu
+## ---------------------------
+
 import numpy as np 
 from numpy.random import Generator,PCG64
 
 def random_3state_ic(rgen,Lx,Ly):
+    ''' Generates an matrix with a set percentage of being -1, 0, or 1
+    '''
     xlow = .49                  # With this choice, only 2 percent of the sites are free
     xhigh = .51
     x = rgen.random((Lx, Ly))           # Generates a number between 0 and 1
@@ -9,6 +19,8 @@ def random_3state_ic(rgen,Lx,Ly):
     return Spin
 
 def random_2state_ic(rgen,Lx,Ly):
+    ''' Generates an matrix with a set percentage of being -1, 1
+    '''
     xlow = .50 
    
     x = rgen.random((Lx, Ly))           # Generates a number between 0 and 1
@@ -16,7 +28,8 @@ def random_2state_ic(rgen,Lx,Ly):
     return Spin
 
 def checkerboard_ic(Lx,Ly,parity=0):
-    '''Create either even or odd checkerboard pattern'''
+    '''Create either even or odd checkerboard pattern with 1s and -1s
+    '''
     mask = np.zeros([Lx,Ly],dtype = np.int)
     for i in range(Lx):
         for j in range(Ly):
